866a4a490cdd4fd525400b3dc3f8e389d7f4a02a
Valentin Huber <git@valentinhuber.me>
2024-01-04 16:56:31 +0000

    updating harness to collect coverage data

--- build-stages.sh
+++
@@ -1,5 +0,0 @@
-DOCKER_IMAGE_TAG=klee-coreutils
-docker build -t "${DOCKER_IMAGE_TAG}-base" --target klee-coreutils-base . \
-    && docker build -t "${DOCKER_IMAGE_TAG}-gcov" --target klee-coreutils-gcov . \
-    && docker build -t "${DOCKER_IMAGE_TAG}-llvm" --target klee-coreutils-llvm . \
-    && docker build -t "${DOCKER_IMAGE_TAG}" --target exec .
